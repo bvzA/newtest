d2295d32717160320f8b875db1da5aee14a39c12
pana <yoophochana.p@gmail.com>
2020-05-29 21:05:18 +0700

    first commit

---
+++ src/test/java/basic/basicapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.basicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
